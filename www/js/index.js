8bedde7fb1f3c76383b3fb5d8b802c3e71aa9e5e
danieldec <danieldelrioc@gmail.com>
2016-07-19 20:14:29 -0600

    login pagina

--- www/js/index.js
+++ www/js/index.js
@@ -2,24 +2,39 @@ $(document).on('ready',function () {
   // creamos esta variable para guardar el nombreUsuario para convertirlo en mayusculas
   var cadenaUsuario="";
   console.log("estamos con el js del index");
-  $('#inUsuario').on('keyup',function () {
-    cadenaUsuario=$(this).val().toUpperCase();
-    $(this).val(cadenaUsuario);
+  //aquí aplicamos tres eventos al input del nombreUsuario para que nos convierta el texto en mayusculas
+  $('#inUsuario').bind('keyup blur focus',function (e) {
+    if (e.type=="keyup") {
+      cadenaUsuario=$(this).val().toUpperCase();
+      $(this).val(cadenaUsuario);
+    }
+    if (e.type="blur") {
+      cadenaUsuario=$(this).val().toUpperCase();
+      $(this).val(cadenaUsuario);
+    }
+    if (e.type="focus") {
+      cadenaUsuario=$(this).val().toUpperCase();
+      $(this).val(cadenaUsuario);
+    }
   })
   //usamos ajax para enviar datos y logearnos a la página.
   $('#loginForm').on('submit',function (e) {
     var usuario= $('#inUsuario').val();
     var contrasena=$('#inContra').val();
+    console.log(usuario);
     $.post('php/login.php',
     {
       nombreUsuario:usuario,
       contrasenaUsuario:contrasena},
       function(data, status) {
-        alert(data+" " +status);
-        $('#tipoAlerta').html(data).addClass("alert alert-danger text-center").show().fadeOut(10000);
+        if (data=="admin") {
+
+        }else{
+            $('#tipoAlerta').html(data).addClass("alert alert-danger text-center").show().fadeOut(5000);
+        }
+
       }
     );
-    console.log(e.type);
     e.preventDefault();
   })
 });
--- www/php/login.php
+++ www/php/login.php
@@ -4,11 +4,25 @@
   if ($_SERVER["REQUEST_METHOD"]==="POST") {
     $usuario=$_POST['nombreUsuario'];
     $contrasena=$_POST['contrasenaUsuario'];
-    $sql="select nombre_usuario, contrasena,perfil from usuarios where '$usuario'=nombre_usuario";
+    $sql="select nombre_usuario, contrasena, perfil from usuarios where nombre_usuario='$usuario'";
     $resultado=$conexion->query($sql);
     if ($resultado) {
-      echo "exito";
-      
+      $fila=$resultado->fetch_array();
+      if ($usuario==$fila['nombre_usuario']&&$contrasena==$fila['contrasena']) {
+        $perfil=$fila['perfil'];
+        switch ($perfil) {
+          case 'admin':
+              echo $perfil;
+            break;
+
+          default:
+            # code...
+            break;
+        }
+      }
+      else {
+        echo "contraseña y/o usuario incorrecto";
+      }
     }else {
       echo "error (".$conexion->connect_errno.")".mysqli_error($conexion)."".$conexion->connect_error;
     }
