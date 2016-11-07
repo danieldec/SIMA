0d4ae5f49fd0a182420bdbc05079b3e2297a75cd
danieldec <danieldelrioc@gmail.com>
2016-11-07 10:21:38 -0700

    reporte2.php

--- www/php/rh/reporte.php
+++ www/php/rh/reporte.php
@@ -1,13 +1,28 @@
 <?php
   include '../conexion/conexion.php';
-  $consulta="SELECT DISTINCT dln.iddetalle_asistenciaDetList FROM detalle_lista_numorden dln WHERE dln.iddetalle_asistenciaDetList IN(SELECT da.iddetalle_asistencia FROM detalle_asistencia da WHERE da.asistencia_fecha='2016-10-31');";
+  $consulta="SELECT DISTINCT dln.iddetalle_asistenciaDetList FROM detalle_Lista_NumOrden dln WHERE dln.iddetalle_asistenciaDetList IN(SELECT da.iddetalle_asistencia FROM detalle_asistencia da WHERE da.asistencia_fecha='2016-11-07');";
   $resultado=$conexion->query($consulta);
   if (!$resultado) {
     echo "conexion fallida".$conexion->errno."($conexion->error)";
+    exit();
   }
   $idDetaAsis=array();
   while ($fila=$resultado->fetch_object()) {
     $idDetaAsis[]=$fila->iddetalle_asistenciaDetList;
   }
-  count($idDetaAsis);
+  echo count($idDetaAsis);
+  print_r($idDetaAsis);
+  $arrayCaptura=array();
+  foreach ($idDetaAsis as $detAsis) {
+    $consulta="SELECT AVG(c.eficiencia) FROM captura c ,detalle_Lista_NumOrden dln, detalle_asistencia da, empleados e WHERE c.iddetalle_Lista_NumOrdenCap IN (SELECT dln.iddetalle_Lista_NumOrden FROM detalle_Lista_NumOrden dln WHERE dln.iddetalle_asistenciaDetList IN (SELECT da.iddetalle_asistencia FROM detalle_asistencia da WHERE da.asistencia_fecha='2016-11-07')) AND dln.iddetalle_Lista_NumOrden=c.iddetalle_Lista_NumOrdenCap AND da.iddetalle_asistencia=dln.iddetalle_asistenciaDetList AND e.idempleados=da.empleados_idempleados AND da.iddetalle_asistencia='$detAsis';";
+    $resultado=$conexion->query($consulta);
+    if (!$resultado) {
+      echo "Error:".$conexion->errno."($conexion->error)";
+      exit();
+    }
+    while ($fila2=$resultado->fetch_object()) {
+      $arrayCaptura[]=$fila2;
+    }
+  }//FIN DEL FOR EACH
+  print_r($arrayCaptura);
  ?>
---
+++ www/php/rh/reporte2.php
@@ -0,0 +1,52 @@
+<?php
+  include '../conexion/conexion.php';
+  $consulta="SELECT c.fecha,c.hora_inicio,c.hora_final, c.tiempo_muerto,c.eficiencia,e.idempleados, CONCAT_WS(' ',e.nombre,e.apellidos) as Nombre, TIME_FORMAT(SUBTIME(c.hora_final,c.hora_inicio),'%l') as tT FROM captura c ,detalle_Lista_NumOrden dln, detalle_asistencia da, empleados e WHERE c.iddetalle_Lista_NumOrdenCap IN (SELECT dln.iddetalle_Lista_NumOrden FROM detalle_Lista_NumOrden dln WHERE dln.iddetalle_asistenciaDetList IN (SELECT da.iddetalle_asistencia FROM detalle_asistencia da WHERE da.asistencia_fecha='2016-11-07')) AND dln.iddetalle_Lista_NumOrden=c.iddetalle_Lista_NumOrdenCap AND da.iddetalle_asistencia=dln.iddetalle_asistenciaDetList AND e.idempleados=da.empleados_idempleados";
+  $resultado=$conexion->query($consulta);
+  if (!$resultado) {
+    echo "error:".$conexion->errno."($conexion->error)";
+    exit();
+  }
+  $contador=1;
+ ?>
+ <!DOCTYPE html>
+ <html lang="es">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Tabla</title>
+ </head>
+ <body>
+   <table>
+     <thead>
+       <tr>
+         <th>#</th>
+         <th>fecha</th>
+         <th>#empleado</th>
+         <th>Nombre</th>
+         <th>hi</th>
+         <th>hf</th>
+         <th>tm</th>
+         <th>tT</th>
+         <th>efi</th>
+       </tr>
+     </thead>
+     <tbody>
+       <?php while ($fila=$resultado->fetch_object()) {
+         echo "<tr>";
+         echo "<td>".$contador."</td>";
+         echo "<td>".$fila->fecha."</td>";
+         echo "<td>".$fila->idempleados."</td>";
+         echo "<td>".$fila->Nombre."</td>";
+         echo "<td>".$fila->hora_inicio."</td>";
+         echo "<td>".$fila->hora_final."</td>";
+         echo "<td>".$fila->tiempo_muerto."</td>";
+         echo "<td>".$fila->tT."</td>";
+         echo "<td>".$fila->eficiencia."</td>"."</tr>";
+         $contador++;
+       }
+        ?>
+     </tbody>
+   </table>
+ </body>
+ </html>
